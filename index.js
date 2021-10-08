213da6d8b7e297b713469d607b094e226b944376
corbinedwards <corbinedwards@comcast.net>
2021-10-08 06:21:21 -0500

    Set up Handlebars view engine

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
+const handlebars = require("express-handlebars");
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    defaultLayout: 'index',
+    partialsDir: `${__dirname}/views/partials`
+}));
+
+app.use(express.static('public'));
 app.use(express.json());
 app.use(require("./routes/routes"));
 
