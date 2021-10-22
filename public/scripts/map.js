80482939a03c0a5f00071796dd23b40f6bf0ae49
corbinedwards <corbinedwards@comcast.net>
2021-10-21 19:45:51 -0500

    Map options

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,46 +1,83 @@
+let map;
+let service;
+let getNextPage;
+const moreButton = document.getElementById("more");
+
 function initMap() {
     const myLatlng = { lat: 36.053950, lng: -94.194576 }
-    const circle = {center: myLatlng, radius: 11265}
-    const map = new google.maps.Map(document.getElementById("map"), {
+
+    map = new google.maps.Map(document.getElementById("map"), {
         center: myLatlng,
         zoom: 17,
     });
-    const marker = new google.maps.Marker({
-        position: myLatlng,
-        map,
-        title: "Click to zoom",
-    });
-    map.addListener("click", () => {
-        console.log("Map Click");
-    });
+    
+    // map.addListener("click", () => {
+    //     console.log("Map Click");
+    // });
 
-    let service = new google.maps.places.PlacesService(map);
-    let request = {
-        query: 'McDonald\'s',
-        fields: ['name', 'geometry'],
-        locationBias: circle
+    moreButton.onclick = function () {
+        moreButton.disabled = true;
+        if (getNextPage) {
+          getNextPage();
+        }
     };
-    service.findPlaceFromQuery(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-        map.setCenter(results[0].geometry.location);
-        }
-    });
 
+    let request = {
+        location: myLatlng,
+        radius: '11265',
+        type: ['restaurant']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, searchCallback);
 }
 
-function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
+function searchCallback(results, status, pagination) {
+    if (status !== "OK" || !results) return;
+    
+    // console.log(results);
+    
+    const restaurantList = document.getElementById("restaurants");
+    
+    const restaurants = results.sort((a, b) => (a.name >= b.name) ? 1 : -1);
+    console.log(restaurants);
+    for (const restaurant of restaurants) {
+        if (restaurant.geometry && restaurant.geometry.location && !document.getElementById(restaurant.name)) {
+          
+            new google.maps.Marker({
+                map,
+                title: restaurant.name,
+                position: restaurant.geometry.location,
+            });
+          
+            const li = document.createElement("li");
+            
+            li.id = restaurant.name;
+            li.className = "restaurant-option"
+            li.textContent = restaurant.name;
+            restaurantList.appendChild(li);
+            li.addEventListener("click", () => {
+                map.setCenter(restaurant.geometry.location);
+            });
+        }
+    }
 
-    const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-    });
-
-    google.maps.event.addListener(marker, "click", () => {
-        infowindow.setContent(place.name || "");
-        infowindow.open(map);
-    });
+    moreButton.disabled = !pagination || !pagination.hasNextPage;
+    
+    if (pagination && pagination.hasNextPage) {
+        getNextPage = () => {
+          // Note: nextPage will call the same handler function as the initial call
+          pagination.nextPage();
+        };
+    }
 }
+
+// function createMarker(place) {
+//     if (!place.geometry || !place.geometry.location) return;
+    
+//     const marker = new google.maps.Marker({
+//         map,
+//         position: place.geometry.location,
+//     });
+    
+// }
